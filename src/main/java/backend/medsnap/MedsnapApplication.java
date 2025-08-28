7d531728201b1c99fd500f9ff6e621560caaa7b3
yuripbong <kyri200350@gmail.com>
2025-08-29 01:57:22 +0900

    Initial commit

---
+++ src/main/java/backend/medsnap/MedsnapApplication.java
@@ -0,0 +1,13 @@
+package backend.medsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsnapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedsnapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/medsnap/MedsnapApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.medsnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsnapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
