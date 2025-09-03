ec011c5194b57f49f12d82b5a691e1f73bde02d7
yuripbong <kyri200350@gmail.com>
2025-09-03 23:12:54 +0900

    feat: 공통 엔티티 작성

--- src/main/java/backend/medsnap/MedsnapApplication.java
+++ src/main/java/backend/medsnap/MedsnapApplication.java
@@ -2,8 +2,10 @@ package backend.medsnap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MedsnapApplication {
 
     public static void main(String[] args) {
