e16dfb2c4fe62fd89fc2190083fcb7fe41a74558
yuripbong <kyri200350@gmail.com>
2025-09-11 19:06:35 +0900

    docs: 배포 스웨거 설정

---
+++ src/main/java/backend/medsnap/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package backend.medsnap.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${https.server.url}")
+    private String httpsServerUrl;
+
+    @Bean
+    public OpenAPI openAPI () {
+        Info info = new Info().title("MEDSNAP API Docs").version("1.0").description("API 명세서");
+
+        // 서버 URL 설정
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("MEDSNAP 로컬 서버");
+
+
+        Server httpsServer = new Server();
+        httpsServer.setUrl(httpsServerUrl);
+        httpsServer.setDescription("MEDSNAP https 서버");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(localServer, httpsServer));
+    }
+}
