eba991deb71689a075b6ba206a86627ac5b1b2e4
yuripbong <kyri200350@gmail.com>
2025-09-17 21:01:50 +0900

    feat: FAQ 엔티티

---
+++ src/main/java/backend/medsnap/domain/faq/entity/Faq.java
@@ -0,0 +1,36 @@
+package backend.medsnap.domain.faq.entity;
+
+import backend.medsnap.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "faqs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Faq extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String answer;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FaqCategory category;
+
+    @Builder
+    public Faq(String question, String answer, FaqCategory category) {
+        this.question = question;
+        this.answer = answer;
+        this.category = category;
+    }
+}
