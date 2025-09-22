48daa411704a5e2bf0961f70247e06fb3ab22c7d
yuripbong <kyri200350@gmail.com>
2025-09-23 03:14:04 +0900

    docs: 토큰 스웨거

--- src/main/java/backend/medsnap/domain/auth/dto/token/TokenPair.java
+++ src/main/java/backend/medsnap/domain/auth/dto/token/TokenPair.java
@@ -1,12 +1,17 @@
 package backend.medsnap.domain.auth.dto.token;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Schema(description = "자체 JWT 토큰 쌍")
 @Getter
 @RequiredArgsConstructor
 public class TokenPair {
 
+    @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
     private final String accessToken;
+
+    @Schema(description = "리프레시 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
     private final String refreshToken;
 }
