fc2d4fc0b1fb920a60278424d61b8fed8918dab1
yuripbong <kyri200350@gmail.com>
2025-09-23 04:29:05 +0900

    refactor: jwk

--- src/main/java/backend/medsnap/infra/oauth/verifier/KakaoOidcVerifier.java
+++ src/main/java/backend/medsnap/infra/oauth/verifier/KakaoOidcVerifier.java
@@ -2,11 +2,13 @@ package backend.medsnap.infra.oauth.verifier;
 
 import backend.medsnap.infra.oauth.discovery.OidcDiscoveryClient;
 import backend.medsnap.infra.oauth.discovery.OidcDiscoveryProperties;
+import backend.medsnap.infra.oauth.exception.JwkProviderInitializationException;
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +23,7 @@ public class KakaoOidcVerifier extends AbstractOidcVerifier {
             @Value("${kakao.client-id}") String clientId,
             OidcDiscoveryClient discoveryClient
     ) {
-        super(clientId);
+        super(new String[]{clientId});
 
         String discoveryUrl = "https://kauth.kakao.com/.well-known/openid-configuration";
         OidcDiscoveryProperties props = discoveryClient.fetch(discoveryUrl);
@@ -33,10 +35,14 @@ public class KakaoOidcVerifier extends AbstractOidcVerifier {
                 ? new String[] { "RS256" }
                 : algs.toArray(new String[0]);
 
-        this.jwkProvider = new JwkProviderBuilder(props.getJwks_uri())
-                .cached(10, 1, TimeUnit.HOURS) // 1시간 캐싱
-                .rateLimited(10, 1, TimeUnit.MINUTES) // 분당 10회 요청 제한
-                .build();
+        try {
+            this.jwkProvider = new JwkProviderBuilder(new URL(props.getJwks_uri()))
+                    .cached(10, 1, TimeUnit.HOURS)
+                    .rateLimited(10, 1, TimeUnit.MINUTES)
+                    .build();
+        } catch (Exception e) {
+            throw new JwkProviderInitializationException(e);
+        }
     }
 
     @Override
