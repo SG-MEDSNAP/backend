f474494ee1d723f5a00232f1ab498fa0da9b5dc0
yuripbong <kyri200350@gmail.com>
2025-09-25 20:07:26 +0900

    feat: JWT 토큰 유효성 검증 클래스 구현

---
+++ src/main/java/backend/medsnap/domain/auth/jwt/JwtTokenValidator.java
@@ -0,0 +1,42 @@
+package backend.medsnap.domain.auth.jwt;
+
+import backend.medsnap.domain.auth.exception.InvalidJwtTokenException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtTokenValidator {
+
+    private final Algorithm algorithm;
+    private final JWTVerifier verifier;
+
+    public JwtTokenValidator(Algorithm algorithm, String issuer) {
+        this.algorithm = algorithm;
+        this.verifier = JWT.require(algorithm)
+                .withIssuer(issuer)
+                .acceptLeeway(5) // 시계 오차 허용 (5초)
+                .build();
+    }
+
+    // JWT 토큰 검증 및 디코딩
+    public DecodedJWT validateToken(String token) {
+        try {
+            return verifier.verify(token);
+        } catch (JWTVerificationException e) {
+            log.error("JWT 검증 실패: {}", e.getMessage());
+            throw new InvalidJwtTokenException();
+        }
+    }
+
+    // 토큰에서 사용자 ID 추출
+    public Long getUserIdFromToken(String token) {
+        DecodedJWT decodedJWT = validateToken(token);
+        return Long.valueOf(decodedJWT.getSubject());
+    }
+}
