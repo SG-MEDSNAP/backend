07b277b81b4860186072a344f83a4ba338853b5f
yuripbong <kyri200350@gmail.com>
2025-09-25 22:35:17 +0900

    refactor: 권한 컬렉션 NPE 방지

--- src/main/java/backend/medsnap/domain/auth/dto/token/CustomUserDetails.java
+++ src/main/java/backend/medsnap/domain/auth/dto/token/CustomUserDetails.java
@@ -2,17 +2,22 @@ package backend.medsnap.domain.auth.dto.token;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Builder
 public class CustomUserDetails implements UserDetails {
 
+    @NonNull
     private final Long id;
-    private final Collection<? extends GrantedAuthority> authorities; // 권한 정보
+    @Builder.Default
+    @NonNull
+    private final Collection<? extends GrantedAuthority> authorities = Collections.emptyList(); // 권한 정보
 
     @Override
     public String getPassword() {
@@ -22,6 +27,11 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public String getUsername() {
         return String.valueOf(id); // 사용자명 -> 사용자 ID
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.unmodifiableCollection(authorities);
     }
 
     @Override
